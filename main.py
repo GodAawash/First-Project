f7917e7dbcdb830579aa9faadc36e630aca3585f
alex <alexphuyal175@gmail.com>
2025-06-12 15:43:11 +0545

    main updatyed

--- main.py
+++ main.py
@@ -156,6 +156,6 @@ async def check_ssl(domain: str):
     return JSONResponse(content=result)
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0",port=9000)
